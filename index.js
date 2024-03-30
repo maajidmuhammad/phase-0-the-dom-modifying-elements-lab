2abbaf45a727a68e3b6d79e8d79f48c7542a22e7
maajidmuhammad <abdimohamed0742@gmail.com>
2024-03-30 18:05:56 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+var mainElement = document.getElementById('main');
+mainElement.parentNode.removeChild(mainElement);
+
+// Create a new 'h1' element
+var newHeader = document.createElement('h1');
+
+// Set the id of the new 'h1' element to 'victory'
+newHeader.setAttribute('id', 'victory');
+
+// Set the text content of the new 'h1' element
+var yourName = "John Doe"; // Replace 'John Doe' with your desired name
+newHeader.textContent = yourName + " is the champion";
+
+// Append the new 'h1' element to the body or any other desired parent element
+document.body.appendChild(newHeader);
